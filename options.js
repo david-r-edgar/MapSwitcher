d05f824da32d1396e6c6a87681fa142d111d2b5c
David Edgar <david.r.edgar@gmail.com>
2016-06-06 16:35:49 +0100

    Add options page. Change favicon.

---
+++ options.js
@@ -0,0 +1,37 @@
+function save_options() {
+    var mapChecks = {};
+    $("#mapsTickList input").each(function() {
+        mapChecks[$(this).attr("id")] = $(this).is(':checked');
+    });
+    chrome.storage.sync.set(mapChecks, function() {
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 1500);
+    });
+}
+
+function restore_options() {
+    var mapDefaults = {};
+    for (outputMap of outputMaps) {
+        mapEntry =
+            "<li>" +
+            "<label for\"" + outputMap.id + "\"><img src=\"image/" + outputMap.image + "\">"  + outputMap.site + "</label>" +
+            "<input type=\"checkbox\" id=\"" + outputMap.id + "\" />" +
+            "</li>";
+            $("#mapsTickList").append(mapEntry);
+        mapDefaults[outputMap.id] = true;
+    }
+    //$("#mapsTickList input").each(function() {
+    //    mapDefaults[$(this).attr("id")] = true;
+    //});
+    chrome.storage.sync.get(mapDefaults, function(items) {
+        console.log(items);
+        $("#mapsTickList input").each(function() {
+            $(this).prop('checked', items[$(this).attr("id")]);
+        });
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
---
+++ outputMaps.js
@@ -0,0 +1,157 @@
+let outputMaps = [
+{
+    site: "Google",
+    image: "googleMapsLogo16x16.png",
+    id: "google",
+    maplinks:
+    {
+        googlemaps: "Maps",
+        googleterrain: "Terrain",
+        googleearth: "Earth"
+    },
+    generate: function(sourceMapData) {
+        var googleBase = "https://www.google.co.uk/maps/";
+        var directions = "";
+        var mapCentre = "@" + sourceMapData.centreCoords.lat + "," + sourceMapData.centreCoords.lng + ",";
+        var zoom = "14z";
+
+        if ("directions" in sourceMapData) {
+            directions = "dir/" + sourceMapData.directions.from + "/" + sourceMapData.directions.to + "/";
+        }
+
+        availableLinks["googlemaps"] = googleBase + directions + mapCentre + zoom;
+        availableLinks["googleterrain"] = googleBase + directions + mapCentre + zoom + "/data=!5m1!1e4";
+        availableLinks["googleearth"] = googleBase + directions + mapCentre + "1891m/data=!3m1!1e3!5m1!1e4";
+    }
+},
+{
+    site: "Bing",
+    image: "bingLogo16x16.png",
+    id: "bing",
+    maplinks:
+    {
+        bingroad: "Road",
+        bingaerial: "Aerial",
+        bingbirdseye: "Bird's eye",
+        bingos: "Ordnance Survey"
+    },
+    generate: function(sourceMapData) {
+        var bingBase = "http://bing.com/maps/default.aspx?";
+        var directions = "";
+        var mapCentre = "cp=" + sourceMapData.centreCoords.lat + "~" + sourceMapData.centreCoords.lng;
+
+        if ("directions" in sourceMapData) {
+            var mode = "";
+            if (sourceMapData.directions.mode) {
+                switch (sourceMapData.directions.mode) {
+                    case "foot":
+                        mode = "&mode=w";
+                        break;
+                    case "car":
+                        mode = "&mode=d";
+                        break;
+                    case "transit":
+                        mode = "&mode=t";
+                        break;
+                }
+            }
+            directions = "rtp=adr." + sourceMapData.directions.from + "~adr." + sourceMapData.directions.to + mode;
+        }
+
+        availableLinks["bingroad"] = bingBase + directions + "&" + mapCentre;
+        availableLinks["bingos"] = bingBase + directions + "&" + mapCentre + "&sty=s";
+        availableLinks["bingaerial"] = bingBase + directions + "&" + mapCentre + "&sty=h";
+        availableLinks["bingbirdseye"] = bingBase + directions + "&" + mapCentre + "&sty=b";
+    }
+},
+{
+    site: "OpenStreetMap",
+    image: "osmLogo16x16.png",
+    id: "osm",
+    maplinks:
+    {
+        osmStandard: "Standard",
+        osmCycle: "Cycle Map",
+        osmTransport: "Transport",
+        osmMapQuestOpen: "MapQuest Open",
+        osmHumanitarian: "Humanitarian"
+    },
+    generate: function(sourceMapData) {
+        var osmBase = "https://www.openstreetmap.org/#map=";
+        var zoom = "12/";
+        var mapCentre = sourceMapData.centreCoords.lat + "/" + sourceMapData.centreCoords.lng;
+
+        availableLinks["osmStandard"] = osmBase + zoom + mapCentre;
+        availableLinks["osmCycle"] = osmBase + zoom + mapCentre + "&layers=C";
+        availableLinks["osmTransport"] = osmBase + zoom + mapCentre + "&layers=T";
+        availableLinks["osmMapQuestOpen"] = osmBase + zoom + mapCentre + "&layers=Q";
+        availableLinks["osmHumanitarian"] = osmBase + zoom + mapCentre + "&layers=H";
+    }
+},
+{
+    site: "Wikimedia Labs",
+    image: "wmLabsLogo16x16.png",
+    id: "wmLabs",
+    maplinks:
+    {
+        wmGeoHack: "GeoHack",
+        wikiminiatlas: "Wiki Mini Atlas"
+    },
+    generate: function(sourceMapData) {
+        var geohackBase = "https://tools.wmflabs.org/geohack/geohack.php?params=";
+        var mapCentre = sourceMapData.centreCoords.lat + "_N_" + sourceMapData.centreCoords.lng + "_E";
+        availableLinks["wmGeoHack"] = geohackBase + mapCentre;
+
+        var wikiminiatlasBase = "https://wma.wmflabs.org/iframe.html?";
+        mapCentre = sourceMapData.centreCoords.lat + "_" + sourceMapData.centreCoords.lng;
+        zoom = "10";
+        availableLinks["wikiminiatlas"] = wikiminiatlasBase + mapCentre + "_0_0_en_" + zoom + "_englobe=Earth";
+    }
+},
+{
+    site: "Wikimapia",
+    image: "wikimapiaLogo16x16.png",
+    id: "wikimapia",
+    maplinks:
+    {
+        wikimapiaSatellite: "Satellite",
+        wikimapiaMap: "Map"
+    },
+    generate: function(sourceMapData) {
+        var wikimapiaBase = "http://wikimapia.org/#lang=en&";
+        var mapCentre = "lat=" + sourceMapData.centreCoords.lat + "&lon=" + sourceMapData.centreCoords.lng;
+        var zoom = "z=12";
+        availableLinks["wikimapiaSatellite"] = wikimapiaBase + mapCentre + '&' + zoom + "&m=b"; //m=b seems to be an optional default anyway
+        availableLinks["wikimapiaMap"] = wikimapiaBase + mapCentre + '&' + zoom + "&m=w";
+    }
+},
+{
+    site: "Geocaching",
+    image: "geocachingLogo16x16.png",
+    id: "geocaching",
+    maplinks:
+    {
+        geocaching: "Map"
+    },
+    generate: function(sourceMapData) {
+        var geocachingBase = "https://www.geocaching.com/map/#?";
+        var mapCentre = "ll=" + sourceMapData.centreCoords.lat + "," + sourceMapData.centreCoords.lng;
+        var zoom = "z=14";
+        availableLinks["geocaching"] = geocachingBase + mapCentre + '&' + zoom;
+    }
+},
+{
+    site: "what3words",
+    image: "w3wLogo.png",
+    id: "w3w",
+    maplinks:
+    {
+        what3words: "Map"
+    },
+    generate: function(sourceMapData) {
+        var w3wBase = "https://map.what3words.com/";
+        var mapCentre = sourceMapData.centreCoords.lat + "," + sourceMapData.centreCoords.lng;
+        availableLinks["what3words"] = w3wBase + mapCentre;
+    }
+}
+];
