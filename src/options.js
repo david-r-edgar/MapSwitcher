c4f11f3744b39128331353f0ce314b038206c0a7
David Edgar <david.r.edgar@gmail.com>
2016-09-16 21:06:48 +0100

    Add select all/select none box to options page.

--- src/options.js
+++ src/options.js
@@ -1,13 +1,36 @@
+
+
+function updateSelectAllNone() {
+    var totalServices = $("#mapsTickList .outpServiceEnabledChk").length;
+    var servicesOn = $("#mapsTickList .outpServiceEnabledChk").filter(function( index ) {
+        return $(this).prop("checked");
+    }).length;
+    if (servicesOn == totalServices) {
+        $("#selectAllNone").prop("checked", true);
+    } else {
+        $("#selectAllNone").prop("checked", false);
+    }
+}
+
+$("#selectAllNone").change(function(ev) {
+    var checked = $("#selectAllNone").prop("checked");
+    $(".chkboxcell .outpServiceEnabledChk").each(function() {
+        $(this).prop("checked", checked);
+    });
+});
+
+
+
 function save_options() {
     var mapChecks = {};
-    $("#mapsTickList input").each(function() {
-        mapChecks[$(this).attr("id")] = $(this).is(':checked');
+    $("#mapsTickList .outpServiceEnabledChk").each(function() {
+        mapChecks[$(this).attr("id")] = $(this).is(":checked");
     });
     chrome.storage.sync.set(mapChecks, function() {
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
+        var status = document.getElementById("status");
+        status.textContent = "Options saved.";
         setTimeout(function() {
-            status.textContent = '';
+            status.textContent = "";
         }, 1500);
     });
 }
@@ -18,16 +41,18 @@ function restore_options() {
         mapEntry =
             "<tr>" +
             "<label for\"" + outputMapService.id + "\"><td class='imgcell'><img src=\"../image/" + outputMapService.image + "\"></td>"  + "<td class='mapnamecell'>" + outputMapService.site + "</td></label>" +
-            "<td class='chkboxcell'><input type=\"checkbox\" id=\"" + outputMapService.id + "\" /></td>" +
+            "<td class='chkboxcell'><input type=\"checkbox\" class='outpServiceEnabledChk' id=\"" + outputMapService.id + "\" /></td>" +
             "</tr>";
             $("#mapsTickList").append(mapEntry);
         mapDefaults[outputMapService.id] = true;
     }
     chrome.storage.sync.get(mapDefaults, function(items) {
-        $("#mapsTickList input").each(function() {
-            $(this).prop('checked', items[$(this).attr("id")]);
+        $("#mapsTickList .outpServiceEnabledChk").each(function() {
+            $(this).prop("checked", items[$(this).attr("id")]);
         });
+        updateSelectAllNone();
     });
+    $("#mapsTickList .outpServiceEnabledChk").change(updateSelectAllNone);
 }
 $(document).ready(restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.getElementById("save").addEventListener("click", save_options);
