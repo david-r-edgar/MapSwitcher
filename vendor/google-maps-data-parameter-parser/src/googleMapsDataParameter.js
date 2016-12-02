08735a573528830c0eb27312b714b1ea061124aa
David Edgar <david.r.edgar@gmail.com>
2016-12-02 20:05:53 +0000

    update gmdp

--- vendor/google-maps-data-parameter-parser/src/googleMapsDataParameter.js
+++ vendor/google-maps-data-parameter-parser/src/googleMapsDataParameter.js
@@ -226,7 +226,7 @@ var Gmdp = function(url) {
     this.mapType = "map";
 
     if (this.prBufRoot == null) {
-        throw new GmdpException("no parsable data parameter");
+        throw new GmdpException("no parsable data parameter found");
     }
 
     //the main top node for routes is 4m; other urls (eg. streetview) feature 3m etc.
@@ -265,39 +265,16 @@ var Gmdp = function(url) {
 
             for (primaryChild of directions.getChildren()) {
                 if (primaryChild.id() == 1 && primaryChild.type() == 'm') {
-                    var addedPrimaryWpt = false;
-                    var wptNodes = primaryChild.getChildren();
-                    for (wptNode of wptNodes) {
-                        if (wptNode.id() == 2) {
-                            //this is the primary wpt, add coords
-                            var coordNodes = wptNode.getChildren();
-                            if (coordNodes &&
-                                coordNodes.length >= 2 &&
-                                coordNodes[0].id() == 1 &&
-                                coordNodes[0].type() == 'd' &&
-                                coordNodes[1].id() == 2 &&
-                                coordNodes[1].type() == 'd') {
-                                    this.route.pushWaypoint(
-                                        new GmdpWaypoint(coordNodes[1].value(),
-                                                         coordNodes[0].value(),
-                                                         true));
-                            }
-                            addedPrimaryWpt = true;
-                        } else if (wptNode.id() == 3) {
-                            //this is a secondary (unnamed) wpt
-                            //
-                            //but first, if we haven't yet added the primary wpt,
-                            //then the coordinates are apparently not specified,
-                            //so we should add an empty wpt
-                            if (!addedPrimaryWpt) {
-                                this.route.pushWaypoint(new GmdpWaypoint(undefined, undefined, true));
-                                addedPrimaryWpt = true;
-                            }
-
-                            //now proceed with the secondary wpt itself
-                            var secondaryWpts = wptNode.getChildren();
-                            if (secondaryWpts && secondaryWpts.length > 1) {
-                                var coordNodes = secondaryWpts[0].getChildren();
+                    if (primaryChild.value() == 0) {
+                        this.route.pushWaypoint(new GmdpWaypoint(undefined, undefined, true));
+                    }
+                    else {
+                        var addedPrimaryWpt = false;
+                        var wptNodes = primaryChild.getChildren();
+                        for (wptNode of wptNodes) {
+                            if (wptNode.id() == 2) {
+                                //this is the primary wpt, add coords
+                                var coordNodes = wptNode.getChildren();
                                 if (coordNodes &&
                                     coordNodes.length >= 2 &&
                                     coordNodes[0].id() == 1 &&
@@ -306,13 +283,39 @@ var Gmdp = function(url) {
                                     coordNodes[1].type() == 'd') {
                                         this.route.pushWaypoint(
                                             new GmdpWaypoint(coordNodes[1].value(),
-                                                             coordNodes[0].value(),
-                                                             false));
+                                                            coordNodes[0].value(),
+                                                            true));
+                                }
+                                addedPrimaryWpt = true;
+                            } else if (wptNode.id() == 3) {
+                                //this is a secondary (unnamed) wpt
+                                //
+                                //but first, if we haven't yet added the primary wpt,
+                                //then the coordinates are apparently not specified,
+                                //so we should add an empty wpt
+                                if (!addedPrimaryWpt) {
+                                    this.route.pushWaypoint(new GmdpWaypoint(undefined, undefined, true));
+                                    addedPrimaryWpt = true;
+                                }
+
+                                //now proceed with the secondary wpt itself
+                                var secondaryWpts = wptNode.getChildren();
+                                if (secondaryWpts && secondaryWpts.length > 1) {
+                                    var coordNodes = secondaryWpts[0].getChildren();
+                                    if (coordNodes &&
+                                        coordNodes.length >= 2 &&
+                                        coordNodes[0].id() == 1 &&
+                                        coordNodes[0].type() == 'd' &&
+                                        coordNodes[1].id() == 2 &&
+                                        coordNodes[1].type() == 'd') {
+                                            this.route.pushWaypoint(
+                                                new GmdpWaypoint(coordNodes[1].value(),
+                                                                coordNodes[0].value(),
+                                                                false));
+                                    }
                                 }
                             }
                         }
-
-
                     }
                 } else if (primaryChild.id() == 3 && primaryChild.type() == 'e') {
                     this.route.setTransportation(primaryChild.value());
@@ -326,6 +329,13 @@ var Gmdp = function(url) {
             if (streetviewChild.id() == 3 && streetviewChild.type() == 'm') {
                 var svInfos = streetviewChild.getChildren();
                 for (svInfo of svInfos) {
+                    if (svInfo.id() == 2 && svInfo.type() == 'e') {
+                        if (svInfo.value() == 4) {
+                            //!2e4!3e11 indicates a photosphere, rather than standard streetview
+                            //but the 3e11 doesn't seem to matter too much (?)
+                            this.mapType = "photosphere";
+                        }
+                    }
                     if (svInfo.id() == 6 && svInfo.type() == 's') {
                         this.svURL = decodeURIComponent(svInfo.value());
                     }
