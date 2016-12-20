d2f7458bc75eb2cd6d60ac58c1f447deebbdbd3b
David Edgar <david.r.edgar@gmail.com>
2016-12-20 23:14:50 +0000

    Remove apparently unneeded code, and parts of the manifest.

--- src/mapSwitcher.js
+++ src/mapSwitcher.js
@@ -386,24 +386,6 @@ var MapSwitcher = {
 
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
-
-
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-
-
-
-
-
 
 /**
  * Entry routine.
