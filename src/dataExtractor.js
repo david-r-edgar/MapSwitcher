59ea2bfde1930ec5520bc72cac5f7544ea1d996d
David Edgar <david.r.edgar@gmail.com>
2019-05-11 19:23:21 +0100

    Fix geocaching map extraction

--- src/dataExtractor.js
+++ src/dataExtractor.js
@@ -339,15 +339,15 @@ extractors.push({
   host: 'geocaching.',
   extract:
     function (resolve) {
-      if (window.location.pathname.indexOf('/map/') === 0) {
+      if (window.location.pathname.indexOf('/map/') >= 0) {
         let sourceMapData = {}
-        const re1 = /ll=([-0-9.]+),([-0-9.]+)/
-        const coordArray = window.location.hash.match(re1)
+        const re1 = /lat=([-0-9.]+)&lng=([-0-9.]+)/
+        const coordArray = window.location.search.match(re1)
         if (coordArray && coordArray.length >= 3) {
           sourceMapData.centreCoords = { 'lat': coordArray[1], 'lng': coordArray[2] }
         }
-        const re2 = /z=([0-9]+)/
-        const zoomArray = window.location.hash.match(re2)
+        const re2 = /zoom=([0-9]+)/
+        const zoomArray = window.location.search.match(re2)
         if (zoomArray && zoomArray.length > 1) {
           sourceMapData.resolution = calculateResolutionFromStdZoom(
             zoomArray[1], sourceMapData.centreCoords.lat)
