04d047555c6debac20636c76072bf67183925e7d
David Edgar <david.r.edgar@gmail.com>
2019-10-13 16:28:00 +0100

    Tabs but all actual content still in only one of them

--- src/mapSwitcher.js
+++ src/mapSwitcher.js
@@ -465,6 +465,29 @@ var MapSwitcher = {
   }
 }
 
+window.addEventListener('load', function () {
+  const myTabs = document.querySelectorAll('ul.nav-tabs > li')
+  function myTabClicks (tabClickEvent) {
+    for (let i = 0; i < myTabs.length; i++) {
+      myTabs[i].classList.remove('active')
+    }
+    const clickedTab = tabClickEvent.currentTarget
+    clickedTab.classList.add('active')
+    tabClickEvent.preventDefault()
+    var myContentPanes = document.querySelectorAll('.tab-pane')
+    for (let i = 0; i < myContentPanes.length; i++) {
+      myContentPanes[i].classList.remove('active')
+    }
+    const anchorReference = tabClickEvent.target
+    const activePaneId = anchorReference.getAttribute('href')
+    const activePane = document.querySelector(activePaneId)
+    activePane.classList.add('active')
+  }
+  for (let i = 0; i < myTabs.length; i++) {
+    myTabs[i].addEventListener('click', myTabClicks)
+  }
+})
+
 /**
  * Entry routine.
  *
