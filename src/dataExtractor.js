8868618f23a0a46531e852d1b4f733d7dc840c73
Joshua Horowitz <joshuah@alum.mit.edu>
2020-04-24 01:48:16 -0700

    Fix lint errors

--- src/dataExtractor.js
+++ src/dataExtractor.js
@@ -896,32 +896,34 @@ extractors.push({
             centreCoords: { lat: coordArray[3], lng: coordArray[2] },
             resolution: calculateResolutionFromStdZoom(coordArray[1], coordArray[3])
           })
-          return true;
+          return true
         }
       }
       if (urlMethod()) { return }
 
       function tileMethod () {
-        const container = document.querySelector(".leaflet-container")
+        const container = document.querySelector('.leaflet-container')
         if (!container) { return false }
         const containerRect = container.getBoundingClientRect()
 
-        const tile = container.querySelector(".leaflet-tile")
+        const tile = container.querySelector('.leaflet-tile')
         if (!tile) { return false }
         const tileRect = tile.getBoundingClientRect()
 
         const re = /\/([0-9]+)\/([0-9]+)\/([0-9]+)@2x.png/
         const coordArray = tile.src.match(re)
         if (!coordArray) { return false }
-        const z = +coordArray[1], tx = +coordArray[2], ty = +coordArray[3]
+        const z = +coordArray[1]
+        const tx = +coordArray[2]
+        const ty = +coordArray[3]
 
         // tx & ty represent the top left of the tile. We now find the center of the map.
         const cx = tx + ((containerRect.left + containerRect.right) / 2 - tileRect.left) / tileRect.width
         const cy = ty + ((containerRect.top + containerRect.bottom) / 2 - tileRect.top) / tileRect.height
-        const lat = Math.atan(Math.sinh(Math.PI*(1 - 2*(cy/2**z)))) / Math.PI * 180
-        const lng = (cx/2**z)*360 - 180
+        const lat = Math.atan(Math.sinh(Math.PI * (1 - 2 * (cy / 2 ** z)))) / Math.PI * 180
+        const lng = (cx / 2 ** z) * 360 - 180
         resolve({
-          centreCoords: {lat, lng},
+          centreCoords: { lat, lng },
           resolution: calculateResolutionFromStdZoom(z, lat)
         })
         return true
