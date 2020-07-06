834e373e19f9ba92c06a5b218d2163e6b752ff25
David Edgar <david.r.edgar@gmail.com>
2020-07-06 16:56:11 +0100

    Support Map With AI RapiD

--- src/dataExtractor.js
+++ src/dataExtractor.js
@@ -1440,6 +1440,21 @@ extractors.push({
     }
 })
 
+extractors.push({
+  host: 'mapwith.ai',
+  extract:
+    function (resolve) {
+      let sourceMapData = {}
+      const re = /map=([0-9.]+)\/([-0-9.]+)\/([-0-9.]+)/
+      const [, zoom, lat, lng] = window.location.hash.match(re)
+      if (lat && lng && zoom) {
+        sourceMapData.centreCoords = { lat, lng }
+        sourceMapData.resolution = calculateResolutionFromStdZoom(zoom, lat)
+      }
+      resolve(sourceMapData)
+    }
+})
+
 var runDataExtraction = function () {
   // default null extractor
   let extractor = {
