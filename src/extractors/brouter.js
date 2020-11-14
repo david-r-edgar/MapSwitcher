f1b4f1cd9d1dde7be31d1517a4a24d5586cd61ef
David Edgar <david.r.edgar@gmail.com>
2020-11-14 21:44:48 +0000

    support extraction of mode of transport for BRouter routes

--- src/extractors/brouter.js
+++ src/extractors/brouter.js
@@ -11,15 +11,35 @@ registerExtractor(resolve => {
     sourceMapData.resolution = calculateResolutionFromStdZoom(zoom, lat)
   }
 
-  const routeRe = /lonlats=([-0-9.,;]+)/
-  const routeReArr = window.location.hash.match(routeRe)
-  if (routeReArr && routeReArr.length > 1) {
-    sourceMapData.directions = {}
-    sourceMapData.directions.route = []
-    routeReArr[1].split(';').forEach(lonlat => {
-      const [lng, lat] = lonlat.split(',')
-      sourceMapData.directions.route.push({ coords: { lat, lng } })
-    })
+  try {
+    const [, coordPairs] = window.location.hash.match(/lonlats=([-0-9.,;]+)/)
+    if (coordPairs) {
+      sourceMapData.directions = {
+        mode: 'bike', // default (if no profile specified)
+        route: []
+      }
+      coordPairs.split(';').forEach(coord => {
+        const [lng, lat] = coord.split(',')
+        sourceMapData.directions.route.push({ coords: { lat, lng } })
+      })
+      const profileMatch = window.location.hash.match(/profile=([a-zA-Z-]+)/)
+      if (profileMatch) {
+        const [, profile] = profileMatch
+        if (profile.includes('bike')) {
+          sourceMapData.directions.mode = 'bike'
+        } else if (profile.includes('velomobile')) {
+          sourceMapData.directions.mode = 'bike'
+        } else if (profile.includes('hiking')) {
+          sourceMapData.directions.mode = 'foot'
+        } else if (profile.includes('rail')) {
+          sourceMapData.directions.mode = 'transit'
+        } else if (profile.includes('car')) {
+          sourceMapData.directions.mode = 'car'
+        }
+      }
+    }
+  } catch (err) {
+    // ignore
   }
 
   resolve(sourceMapData)
