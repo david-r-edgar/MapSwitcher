19c5100da5a09c71289f96da85ba4e32ace891a9
David Edgar <david.r.edgar@gmail.com>
2024-08-04 19:19:30 +0100

    fix bing extraction

--- src/extractors/bing.js
+++ src/extractors/bing.js
@@ -5,41 +5,18 @@
 
 registerExtractor((resolve, reject) => {
   const sourceMapData = {}
-  // if there's no 'state', it means no scrolling has happened yet.
-  // So we should extract the lat and lng from the window.location parameter
-  if (window.history && !window.history.state) {
-    const re1 = /cp=([-0-9.]+)~([-0-9.]+)/
-    const coordArray = window.location.search.match(re1)
 
-    if (coordArray && coordArray.length >= 3) {
-      sourceMapData.centreCoords = { lat: coordArray[1], lng: coordArray[2] }
-    }
-    const re2 = /lvl=([0-9]+)/
-    const levelArray = window.location.search.match(re2)
-    if (levelArray && levelArray.length > 1) {
-      sourceMapData.resolution = calculateResolutionFromStdZoom(
-        levelArray[1], sourceMapData.centreCoords.lat)
-    }
-  } else {
-    // scrolling has happened, but bing doesn't update its URL. So we pull
-    // the coords from the'MapModeStateHistory'
+  const re1 = /cp=([-0-9.]+)%7E([-0-9.]+)/
+  const coordArray = window.location.search.match(re1)
 
-    // MapModeStateHistory broke - so we assume it may be in one of two locations
-    let mapModeStateHistory
-    if (window.history.state && window.history.state.MapModeStateHistory) {
-      mapModeStateHistory = window.history.state.MapModeStateHistory
-    } else if (window.history.state.state && window.history.state.state.MapModeStateHistory) {
-      mapModeStateHistory = window.history.state.state.MapModeStateHistory
-    }
-    if (!mapModeStateHistory) {
-      reject()
-    }
-
-    sourceMapData.centreCoords = { lat: mapModeStateHistory.centerPoint.latitude, lng: mapModeStateHistory.centerPoint.longitude }
-
-    const level = mapModeStateHistory.level
+  if (coordArray && coordArray.length >= 3) {
+    sourceMapData.centreCoords = { lat: coordArray[1], lng: coordArray[2] }
+  }
+  const re2 = /lvl=([0-9.]+)/
+  const levelArray = window.location.search.match(re2)
+  if (levelArray && levelArray.length > 1) {
     sourceMapData.resolution = calculateResolutionFromStdZoom(
-      level, sourceMapData.centreCoords.lat)
+      levelArray[1], sourceMapData.centreCoords.lat)
   }
 
   const directionsPanelRoot = document.evaluate('//*[@id="directionsPanelRoot"]',
